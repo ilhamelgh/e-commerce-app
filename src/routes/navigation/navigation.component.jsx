f98778e26b99d976ff2e6f86da79dd21c88cc368
ilhamelgharbaoui <ielgharbaoui@azura-group.com>
2024-06-20 09:43:10 +0100

    add cart dropdown - wip

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom"
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import './navigation.styles.scss'
@@ -26,7 +29,9 @@ const Navigation = () => {
               <Link className="nav-link" to='/auth' > SIGN IN </Link>
             )
           }
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
